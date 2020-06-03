1a92cb96a7606e92f3352f40501abcd5be7064e8
yasinhajilou <yasinhaji78@yahoo.com>
2020-06-03 11:19:10 +0430

    apk added

--- app/src/main/java/com/yasinhajiloo/washhandsreminder/constants/MySharedPreferenceConstants.java
+++ app/src/main/java/com/yasinhajiloo/washhandsreminder/constants/MySharedPreferenceConstants.java
@@ -3,6 +3,4 @@ package com.yasinhajiloo.washhandsreminder.constants;
 public class MySharedPreferenceConstants {
     public static String sharedPreferenceName = "myPref";
     public static String KEY_LONG_TIME = "key_long_time";
-    public static String KEY_STATUS = "key_status";
-
 }
